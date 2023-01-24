1f8f1b9ea4b587835b352c1c7c84140884940274
giuliaconte <giuliac@outlook.com>
2023-01-24 13:01:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// DOM elements 
+const time = document.getElementById('time'),
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById('name'),
+    goal = document.getElementById('goal');
+
